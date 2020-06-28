041befc58e49e9d582b178d6cb68fd4c0c2930cd
dchiapa <diegochiapa@gmail.com>
2020-06-28 20:05:16 -0300

    v1.0 completa

---
+++ portfolio.php
@@ -0,0 +1,63 @@
+	<?php
+  include 'html/header.html';
+  include 'html/menu.html';
+  ?>
+
+	<main id="portfolio">
+	  <h1>Portfolio</h1>
+	  <section>
+	    <article>
+	      <h2>Juego piedra, papel o tijera</h2>
+	      <p>Proyecto de desarrollo propio, es una app que permite jugar al clasico juego que tradicionalmente jugamos con nuestras manos.</p>
+	      <p>Tecnologias: JS, HTML5, CSS3</p>
+	      <a href="http://piedra-papel-tijera.diegochiapa.com.ar" target="_blank" class="ancla"> Ir al Sitio</a>
+	      <a href="https://github.com/dchiapa/juegoPPT" target="_blank" class="ancla">Repositorio Github</a>
+	    </article>
+	    <article>
+	      <h2>Generador de nombres</h2>
+	      <p>Proyecto del curso "JavaScript Moderno Guia Definitiva", es una app que permite generar una lista de nombre de bebe.</p>
+	      <p>Tecnologias: JS, HTML5, CSS3</p>
+	      <a href="http://nombres.diegochiapa.com.ar" target="_blank" class="ancla"> Ir al Sitio</a>
+	      <a href="https://github.com/dchiapa/JsGeneradorNombres" target="_blank" class="ancla">Repositorio Github</a>
+	    </article>
+	    <article>
+	      <h2>Gasto Semanal</h2>
+	      <p>Proyecto del curso "JavaScript Moderno Guia Definitiva", es una app que permite llevar el control del gasto semanal, a partir de un presupuesto inicial</p>
+	      <p>Tecnologias: JS, HTML5, CSS3</p>
+	      <a href="http://gasto.diegochiapa.com.ar" target="_blank" class="ancla"> Ir al Sitio</a>
+	      <a href="https://github.com/dchiapa/JsGastoSemanal" target="_blank" class="ancla">Repositorio Github</a>
+	    </article>
+	    <article>
+	      <h2>Seguro de Auto</h2>
+	      <p>Proyecto del curso "JavaScript Moderno Guia Definitiva", es una app que permite cotizar el seguro de un auto segun marca, año y tipo de seguro.</p>
+	      <p>Tecnologias: JS, HTML5, CSS3</p>
+	      <a href="http://seguros.diegochiapa.com.ar" target="_blank" class="ancla"> Ir al Sitio</a>
+	      <a href="https://github.com/dchiapa/JsCotizadorSeguro" target="_blank" class="ancla">Repositorio Github</a>
+	    </article>
+	    <article>
+	      <h2>Email</h2>
+	      <p>Proyecto del curso "JavaScript Moderno Guia Definitiva", es una app que ejecuta el front-end del formulario de contacto</p>
+	      <p>Tecnologias: JS, HTML5, CSS3</p>
+	      <a href="http://email.diegochiapa.com.ar" target="_blank" class="ancla"> Ir al Sitio</a>
+	      <a href="https://github.com/dchiapa/JsEmail" target="_blank" class="ancla">Repositorio Github</a>
+	    </article>
+	    <article>
+	      <h2>Notas</h2>
+	      <p>Proyecto del curso "JavaScript Moderno Guia Definitiva", Permite escribir un mensaje y que se muestre en el panel lateral, hasta ahora nada muy sofisticado, pero y si te digo que si cerras el navegador, y cuando vuelvas a la pagina web las notas anteriores van a seguir ahí? Si, las notas quedan guardadas en tu compu, no hay un servidor, no hay envío de datos, solo tu compu y tus notas.</p>
+	      <p>Tecnologias: JS, HTML5, CSS3</p>
+	      <a href="http://notas.diegochiapa.com.ar" target="_blank" class="ancla"> Ir al Sitio</a>
+	      <a href="https://github.com/dchiapa/appNotas" target="_blank" class="ancla">Repositorio Github</a>
+	    </article>
+	    <article>
+	      <h2>simar-srl.com.ar</h2>
+	      <p>Es el sitio web de una empresa, dedicada al comercio exterior, incluye una página de inicio, una página de servicios y un formulario de contacto.</p>
+	      <a href="http://simar-srl.com.ar" target="_blank" class="ancla"> Ir al Sitio</a>
+	    </article>
+	    <article>
+	      <h2>cardistry.com.ar</h2>
+	      <p>Es un sitio que permite subir entradas a un blog, y videos a una seccion independiente, con cuatro secciones incluyendo una página de inicio y un formulario de contacto.</p>
+	      <a href="http://cardistry.diegochiapa.com.ar" target="_blank" class="ancla"> Ir al Sitio</a>
+	    </article>
+	  </section>
+	</main>
+	<?php include 'html/footer.html'; ?>
